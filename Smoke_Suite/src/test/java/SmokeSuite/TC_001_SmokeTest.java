20f2d7b2b37999af44b60c3546cc6ba1a599eac0
hiteshg <hiteshg@PG01CBAC.pyramidconsultinginc.com>
2019-02-18 19:07:06 +0530

    committed

---
+++ Smoke_Suite/src/test/java/SmokeSuite/TC_001_SmokeTest.java
@@ -0,0 +1,42 @@
+package SmokeSuite;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TC_001_SmokeTest {
+	
+	
+	
+	WebDriver driver;
+	@BeforeMethod
+	public void setUp()
+	{
+		WebDriverManager.chromedriver().setup();
+		driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+		driver.get("http://www.google.com");
+	}
+	
+	@Test
+	public void smokeTest()
+	{
+		String url=driver.getCurrentUrl();
+		System.out.println("URL is" +url);
+		
+	}
+	
+	@AfterMethod
+	public void quit()
+	{
+		driver.quit();
+	}
+
+}
